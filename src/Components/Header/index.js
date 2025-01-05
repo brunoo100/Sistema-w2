9b9481d83f644d448f56011f87b881d87613a947
brunoo100 <brunodlucas2@gmail.com>
2025-01-05 17:43:06 -0300

    css

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,11 @@
 import logo from '../../img/image.png';
 import { Link, useNavigate } from 'react-router-dom';
-import { useState, useEffect, useRef } from 'react'; // useRef foi adicionado
+import { useState } from 'react'; // Para gerenciar o estado do menu
 import './header.css';
 import supabase from '../../service/supabase';
 
 export default function Header() {
   const [menuAberto, setMenuAberto] = useState(false); // Estado para controlar a visibilidade do menu
-  const menuRef = useRef(null); // Referência para o menu
   const navigate = useNavigate();
 
   const sair = async () => {
@@ -18,30 +17,13 @@ export default function Header() {
     setMenuAberto(!menuAberto); // Alterna o estado do menu
   };
 
-  // Efeito para detectar cliques fora do menu e fechá-lo
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        setMenuAberto(false); // Fecha o menu se o clique for fora dele
-      }
-    };
-
-    // Adiciona o evento de clique
-    document.addEventListener('mousedown', handleClickOutside);
-
-    // Limpa o evento quando o componente for desmontado
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
   return (
     <div>
       <header>
         <div className="logo">
           <img src={logo} alt="Logo" />
         </div>
-        <div className="menu" ref={menuRef}> {/* Adiciona a referência aqui */}
+        <div className="menu">
           <nav className={menuAberto ? "open" : ""}> {/* Aplica a classe 'open' se o menu estiver aberto */}
             <ul>
               <li><Link to="/home">Home</Link></li>
